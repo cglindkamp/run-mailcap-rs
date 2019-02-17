86d0be5f2c33a73de131e10fd43609725bb3d412
Christian Glindkamp <christian.glindkamp@gmail.com>
2019-02-17 16:47:32 +0100

    Search multiple path for a mime.types file
    The one in the home directory is currently not supported and for tests
    we use a local one from tests folder.

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,30 @@ use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::env;
 
-fn get_mime_type(extension: &str) -> Result<String, io::Error> {
-    let path = Path::new("/etc/mime.types");
+fn get_mime_type(mime_paths: &[&Path], extension: &str) -> Result<String, io::Error> {
+    for path in mime_paths {
+        let file = match File::open(&path) {
+            Ok(file) => file,
+            Err(_e) => continue,
+        };
 
-    let file = match File::open(&path) {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
+        let file = BufReader::new(file);
 
-    let file = BufReader::new(file);
-
-    for line in file.lines() {
-        match line {
-            Ok(line) => {
-                let mut items = line.split_whitespace();
-                let mime = items.nth(0);
-                if let Some(mime) = mime {
-                    for s in line.split_whitespace() {
-                        if extension == s {
-                            return Ok(String::from(mime));
+        for line in file.lines() {
+            match line {
+                Ok(line) => {
+                    let mut items = line.split_whitespace();
+                    let mime = items.nth(0);
+                    if let Some(mime) = mime {
+                        for s in line.split_whitespace() {
+                            if extension == s {
+                                return Ok(String::from(mime));
+                            }
                         }
                     }
                 }
+                Err(e) => return Err(e),
             }
-            Err(e) => return Err(e),
         }
     }
 
@@ -35,18 +35,29 @@ fn get_mime_type(extension: &str) -> Result<String, io::Error> {
 }
 
 fn main() {
-    println!("{}", get_mime_type(&env::args().nth(1).unwrap()).unwrap());
+    let mime_paths: [&Path; 3] = [
+        Path::new("/usr/share/etc/mime.types"),
+        Path::new("/usr/local/etc/mime.types"),
+        Path::new("/etc/mime.types"),
+    ];
+    println!("{}", get_mime_type(&mime_paths, &env::args().nth(1).unwrap()).unwrap());
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::PathBuf;
 
     #[test]
     fn test_mime_types() {
-        assert_eq!(get_mime_type("mp4").unwrap(), "video/mp4");
-        assert_eq!(get_mime_type("txt").unwrap(), "text/plain");
-        assert_eq!(get_mime_type("").unwrap(), "application/octet-stream");
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("tests/data/mime.types");
+
+        let mime_paths: [&Path; 1] = [&path.as_path()];
+
+        assert_eq!(get_mime_type(&mime_paths, "mp4").unwrap(), "video/mp4");
+        assert_eq!(get_mime_type(&mime_paths, "txt").unwrap(), "text/plain");
+        assert_eq!(get_mime_type(&mime_paths, "").unwrap(), "application/octet-stream");
     }
 }
